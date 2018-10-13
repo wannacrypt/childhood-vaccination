1e3c896260845e7c9f84e4e1f47062ce45ca768a
bzhandos <baurzhan.zhandos@bam.de>
2018-10-13 12:42:26 +0600

    without obj

--- Models/Child.cs
+++ Models/Child.cs
@@ -3,6 +3,10 @@ namespace ChildhoodVaccination.Models
     public class Child
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        // public Date dob { get; set; }
+
     }
 }
